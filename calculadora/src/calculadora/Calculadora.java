d9e87fa205bb9801f8c17c0265c84808200ef182
Julián García <118063845+juligarciadevweb@users.noreply.github.com>
2025-09-25 09:36:01 -0300

    Trabajo practico N2 Paradigmas 2do año
    1) Programar una mini Ventana que permite realizar cálculos: Sumas, Productos y otras operaciones entre 2 números. De ser posible, darle el aspecto de una calculadora, con botones para ingresar los números.

---
+++ calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,13 @@
+package calculadora;
+
+public class Calculadora {
+
+    public static void main(String[] args) 
+    {
+        //tengo que instanciar la clase ventana
+        ventana v = new ventana();
+        v.setVisible(true); //le damos el atributo true para que
+        
+    }
+    
+}
