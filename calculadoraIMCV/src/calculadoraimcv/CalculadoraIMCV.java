087c8e5324082237a66191a353f5b04b37ea317b
Julián García <118063845+juligarciadevweb@users.noreply.github.com>
2025-09-25 09:37:28 -0300

    Trabajo practico N2 - Paradigmas de programacion
    Programar una Ventana que permita ingresar la altura y el peso de una persona, y muestre el Índice de masa corporal, indicando si el peso es normal, inferior o superior. Validar, antes de realizar el cálculo, que la altura sea positiva y numérica. Igual con el peso. Mostrar un mensaje de Error en un Diálogo emergente con JOptionPane, indicando el error.

---
+++ calculadoraIMCV/src/calculadoraimcv/CalculadoraIMCV.java
@@ -0,0 +1,18 @@
+package calculadoraimcv;
+
+/*
+ * Programar una Ventana que permita ingresar la altura y el peso de una persona, 
+ * y muestre el Índice de masa corporal, 
+ * indicando si el peso es normal, inferior o superior. 
+ * Validar, antes de realizar el cálculo, que la altura sea positiva y numérica. Igual con el peso. 
+ * Mostrar un mensaje de Error en un Diálogo emergente con JOptionPane, indicando el error.
+ */
+
+public class CalculadoraIMCV 
+{
+    public static void main(String[] args) {
+        Ventana v = new Ventana();
+        v.setVisible(true);
+    }
+    
+}
