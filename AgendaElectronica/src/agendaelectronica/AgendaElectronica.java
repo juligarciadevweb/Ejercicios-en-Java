cf563c2e017ccd37830d6cf21dd6f296bda02f44
Julián García <118063845+juligarciadevweb@users.noreply.github.com>
2025-10-02 11:33:31 -0300

    Agenda electronica - 2do trabajo practico PP
    Agenda electronica con uso de vectores para el almacenado de informacion

---
+++ AgendaElectronica/src/agendaelectronica/AgendaElectronica.java
@@ -0,0 +1,11 @@
+package agendaelectronica;
+
+public class AgendaElectronica {
+
+    public static void main(String[] args) {
+        Principal princ = new Principal();
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);
+    }
+    
+}
